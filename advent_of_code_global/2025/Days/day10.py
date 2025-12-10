1a2a6c1516205fac7204b815b4340f9221d2e99f
Shaun Levy <SLY@ndonline.ac.uk>
2025-12-10 12:26:11 +0000

    somehow day10 part1 works

--- advent_of_code_global/2025/Days/day10.py
+++ advent_of_code_global/2025/Days/day10.py
@@ -1,7 +1,13 @@
+from operator import ne
 from utils.datahandler import read_data
 from math import sqrt, inf
 
-def handle(data):
+def handleInput(data):
+	data = data.split("\n")
+	data = [handleLine(line) for line in data]
+	return data
+
+def handleLine(data):
 	data = data.split(" ")
 	data = [data[0][1:-1], data[1:-1]]
 	data[1] = [tuple([int(num) for num in line[1:-1].split(",")]) for line in data[1]]
@@ -16,7 +22,15 @@ def stateChange(state,input):
 			state[num] = "."
 	return "".join(state)
 
-def part1(data):
+def display(states):
+	for state in states.keys():
+		print(state)
+		start = states[state]
+		for transition in start.keys():
+			end = start[transition]
+			print("\t",transition,"==>",end)
+
+def solve(data):
 	answer = 0
 	endState = data[0]
 	print("end state =",endState)
@@ -30,17 +44,36 @@ def part1(data):
 
 	for state in states.keys():
 		for button in buttons:
-			states[state][button] = stateChange(state, button)
+			end = stateChange(state, button)
+			states[state][button] = end
 
-	for state in states.keys():
-		print(state)
-		start = states[state]
-		for transition in start.keys():
-			end = start[transition]
-			print("\t",transition,"==>",end)
-			if end == endState:
-				print("\t\t^^^^^")
+	current = endState
 
+	queue = [state for state in states.keys()]
+	distances = {state : inf for state in states.keys()}
+	distances[current] = 0
+
+	queue = [state for distance, state in sorted(zip([distances[_] for _ in queue],queue))]
+
+	while current != "."*length:
+		current = queue.pop(0)
+		neighbours = [state for state in states[current].values()]
+		for neighbour in neighbours:
+			oldDistance = distances[neighbour]
+			newDistance = distances[current] + 1
+			if newDistance < oldDistance:
+				distances[neighbour] = newDistance
+
+		queue = [state for distance, state in sorted(zip([distances[_] for _ in queue],queue))]
+	return distances["."*length]
+
+def part1(data):
+	answer = 0
+	for line in data:
+		print(line)
+		answer_ = solve(line)
+		print(answer_)
+		answer += answer_
 	return answer
 
 def part2(data):
@@ -49,7 +82,7 @@ def part2(data):
 	return answer
 
 data = read_data(__file__, "2025")
-data = handle(data)
+data = handleInput(data)
 print(data)
 print(part1(data))
 print(part2(data))
